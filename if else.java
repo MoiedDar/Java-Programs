5b2fc669d0904d2c208b45fdd0951fc66209ae7c
MoiedDar <90909825+MoiedDar@users.noreply.github.com>
2021-12-12 12:09:06 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,14 @@
+class  numbers{
+  public static void main(String[] args) {
+  int a=2, b=2;
+  if (a == b){
+  System.out.println("Both are equal");
+  }
+  else if(a>b){
+  System.out.println("a is greater than b");
+  }
+  else{
+  System.out.println("b is greater than a");
+  }
+  }
+}
