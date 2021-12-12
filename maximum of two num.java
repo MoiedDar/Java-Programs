c80a506ba2db0ea68335ddaded145995482dbb21
MoiedDar <90909825+MoiedDar@users.noreply.github.com>
2021-12-12 12:38:51 +0530

    Create maximum of two num.java

---
+++ maximum of two num.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class numbers {
+   public static void main(String[] args) {
+      //integer values
+      int a = 15;
+      int b = 5;
+      //float values
+      float c = 100.00f;
+      float d = 99.99f;
+      // double values
+      double e = 30.01d;
+      double f = 29.344d;
+      // long values
+      long g = 132534l;
+      long h = 13683l;
+      System.out.println("Maximum of " + a +" and " + b +" is " + Math.max(a,b));
+      System.out.println("Maximum of " + c +" and " + d +" is " + Math.max(c,d));
+      System.out.println("Maximum of " + e +" and " + f +" is " + Math.max(x,y));
+      System.out.println("Maximum of " + g +" and " + h +" is " + Math.max(r,s));
+   }
+}
